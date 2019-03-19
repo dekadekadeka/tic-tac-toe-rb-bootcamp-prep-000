8fb37e789e3238da017176a61f33598c6813f738
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-03-19 19:05:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,7 +95,7 @@ end
 def play(board)
   turn(board) until over?(board)
   if won?(board)
-    puts "Congratulations X!"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
